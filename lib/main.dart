4905789d75d385b9e927704f332c680d613ebad2
Steven Montero <steven.monteromunoz@ucrso.info>
2020-10-15 10:57:40 -0600

    Se descomenta el main xd

--- lib/main.dart
+++ lib/main.dart
@@ -10,104 +10,106 @@ import 'package:lamanda_admin/src/theme/theme.dart';
 
 import 'src/AuthenticationBloc/authentication_bloc.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(MyApp(
-//     authenticationRepository: AuthenticationRepository(),
-//   ));
-// }
-
-// class MyApp extends StatelessWidget {
-//   final AuthenticationRepository authenticationRepository;
-//   const MyApp({
-//     Key key,
-//     @required this.authenticationRepository,
-//   })  : assert(authenticationRepository != null),
-//         super(key: key);
-//   @override
-//   Widget build(BuildContext context) {
-//     //var data = EasyLocalizationProvider.of(context).data;
-//     //Obliga  a la aplicacion a solo funcionar en portrait
-//     SystemChrome.setPreferredOrientations([
-//       DeviceOrientation.portraitUp,
-//       DeviceOrientation.portraitDown,
-//     ]);
-//     //set color status bar
-//     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
-//       statusBarColor: Colors.transparent, //or set color with: Color(0xFF0000FF)
-//     ));
-//     return RepositoryProvider(
-//       create: (context) => AuthenticationBloc(
-//           authenticationRepository: authenticationRepository),
-//       child: AppView(),
-//     );
-
-//     //EasyLocalizationProvider(
-//     //  data: data,
-//     //  child: RepositoryProvider.value(
-//     //    value: authenticationRepository,
-//     //    child: BlocProvider(
-//     //      create: (context) => AuthenticationBloc(
-//     //          authenticationRepository: authenticationRepository),
-//     //      child: AppView(data: data),
-//     //    ),
-//     //  ),
-//     //);
-//   }
-// }
-
-// class AppView extends StatefulWidget {
-//   const AppView({
-//     Key key,
-//     //@required this.data,
-//   }) : super(key: key);
-
-//   //final data;
-
-//   @override
-//   _AppViewState createState() => _AppViewState();
-// }
-
-// class _AppViewState extends State<AppView> {
-//   final _navigatorKey = GlobalKey<NavigatorState>();
-//   NavigatorState get _navigator => _navigatorKey.currentState;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         theme: theme(),
-//         debugShowCheckedModeBanner: false,
-//         navigatorKey: _navigatorKey,
-//         title: 'La Manada petShop',
-//         routes: getRoutesApp(),
-//         builder: (context, child) {
-//           return BlocListener<AuthenticationBloc, AuthenticationState>(
-//             listener: (context, state) {
-//               switch (state.status) {
-//                 case AuthenticationStatus.authenticated:
-//                   _navigator.pushReplacementNamed('home');
-//                   break;
-//                 case AuthenticationStatus.unauthenticated:
-//                   _navigator.pushReplacementNamed('listProducts');
-//                   break;
-//                 default:
-//                   break;
-//               }
-//             },
-//             child: child,
-//           );
-//         },
-//         onGenerateRoute: (settings) =>
-//             MaterialPageRoute(builder: (context) => HomeScreen()));
-//   }
-// }
-
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp(
+    authenticationRepository: AuthenticationRepository(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
+  final AuthenticationRepository authenticationRepository;
+  const MyApp({
+    Key key,
+    @required this.authenticationRepository,
+  })  : assert(authenticationRepository != null),
+        super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Material App', home: ListProducts());
+    //var data = EasyLocalizationProvider.of(context).data;
+    //Obliga  a la aplicacion a solo funcionar en portrait
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    //set color status bar
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.transparent, //or set color with: Color(0xFF0000FF)
+    ));
+    return RepositoryProvider(
+      create: (context) => AuthenticationBloc(
+          authenticationRepository: authenticationRepository),
+      child: AppView(),
+    );
+
+    //EasyLocalizationProvider(
+    //  data: data,
+    //  child: RepositoryProvider.value(
+    //    value: authenticationRepository,
+    //    child: BlocProvider(
+    //      create: (context) => AuthenticationBloc(
+    //          authenticationRepository: authenticationRepository),
+    //      child: AppView(data: data),
+    //    ),
+    //  ),
+    //);
   }
 }
+
+class AppView extends StatefulWidget {
+  const AppView({
+    Key key,
+    //@required this.data,
+  }) : super(key: key);
+
+  //final data;
+
+  @override
+  _AppViewState createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+  NavigatorState get _navigator => _navigatorKey.currentState;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: theme(),
+        debugShowCheckedModeBanner: false,
+        navigatorKey: _navigatorKey,
+        title: 'La Manada petShop',
+        routes: getRoutesApp(),
+        builder: (context, child) {
+          return BlocListener<AuthenticationBloc, AuthenticationState>(
+            listener: (context, state) {
+              switch (state.status) {
+                case AuthenticationStatus.authenticated:
+                  _navigator.pushReplacementNamed('home');
+                  break;
+                case AuthenticationStatus.unauthenticated:
+                  _navigator.pushReplacementNamed('listProducts');
+                  break;
+                default:
+                  break;
+              }
+            },
+            child: child,
+          );
+        },
+        onGenerateRoute: (settings) =>
+            MaterialPageRoute(builder: (context) => HomeScreen()));
+  }
+}
+
+//Debug Main
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(title: 'Material App', home: ListProducts());
+//   }
+// }
