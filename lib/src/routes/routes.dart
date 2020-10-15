cb5e0cd7f56e6163f8a4faffd47ba13ba4678c10
COSME ANDREY MONTERO CHAVES <cosme.montero@ucr.ac.cr>
2020-10-15 09:51:35 -0600

    Diseño gestión de productos

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:lamanda_admin/src/pages/Products/List_Products.dart';
+import 'package:lamanda_admin/src/pages/home.dart';
 
 Map<String, WidgetBuilder> getRoutesApp() {
   return <String, WidgetBuilder>{
-    
+    'home': (BuildContext context) => HomeScreen(),
+    'listProducts': (BuildContext context) => List_Products(),
   };
 }
