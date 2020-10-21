f75ce126166c3f53aecb99a9bf40b5f0ec8cbe0e
Maykel <maykel.trujillo@ucrso.info>
2020-10-20 22:50:50 -0600

    se agrega admin home y la carpeta images en assets

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:lamanda_admin/src/pages/Products/list_products_page.dart';
 import 'package:lamanda_admin/src/pages/home.dart';
+import 'package:lamanda_admin/src/pages/login_pages/login_page.dart';
 
 Map<String, WidgetBuilder> getRoutesApp() {
   return <String, WidgetBuilder>{
-    'home': (BuildContext context) => HomeScreen(),
+    'admin_home': (BuildContext context) => AdminHome(),
     'listProducts': (BuildContext context) => ListProducts(),
+    'login': (BuildContext context) => LoginScreen(),
   };
 }
